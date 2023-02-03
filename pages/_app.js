3ebebe65c178959c51a4509d8f67b78b3fd739b0
Harsh Mathur <harsh@felicity.care>
2023-02-03 08:43:54 +0530

    Google Site Verification Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import { MediumProvider } from '../context/MediumContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <MediumProvider>
+      <Head>
+        <meta name="google-site-verification" content="ufjB4lpFxeLeyrleMdoU3BtoNleUMd-Wb53g-gZzEiQ" />
+      </Head>
       <Component {...pageProps} />
     </MediumProvider>
   )
